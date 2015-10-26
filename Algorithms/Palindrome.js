d4c0783b8c0f46fea52355d46401bf0a02d00736
Ed Gilmore <edgilmore@origamiowl.com>
2015-10-26 15:32:22 -0700

    Added Palindrome to Alogrithms --incomplete

---
+++ Algorithms/Palindrome.js
@@ -0,0 +1,16 @@
+/**
+ * Created by edgilmore on 10/26/2015.
+ */
+//not working yet...
+function palindrome(str) {
+    var regex = '/\s+/gi';
+    str = str.toLowerCase();
+    str = str.replace(regex, '');
+    console.log( str);
+    // Good luck!
+    return true;
+}
+
+
+
+palindrome("eye");
