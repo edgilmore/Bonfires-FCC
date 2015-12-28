8f2b846e14aa8a20426246ea261034c7b537272f
Ed Gilmore <edgilmore@live.com>
2015-12-28 11:50:55 -0700

    Added file for diff two arrays algorithm

---
+++ Intermediate-Algorithm/DiffTwoArrays.js
@@ -0,0 +1,12 @@
+/**
+ * Created by edgilmore on 12/17/2015.
+ * Instructions:
+ */
+'use strict';
+function diff(arr1, arr2) {
+    var newArr = [];
+    // Same, same; but different.
+    return newArr;
+}
+
+diff([1, 2, 3, 5], [1, 2, 3, 4, 5]);
