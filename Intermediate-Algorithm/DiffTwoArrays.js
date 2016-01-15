99e8311d0e781e491a4c464e86d30d0152f72cbc
Ed Gilmore <edgilmore@live.com>
2016-01-15 10:51:07 -0700

    Added solution for DiffTwoArrays

--- Intermediate-Algorithm/DiffTwoArrays.js
+++ Intermediate-Algorithm/DiffTwoArrays.js
@@ -3,20 +3,19 @@
  * Instructions: Compare two arrays and return a new array with any items only found in one of the original arrays.
  */
 'use strict';
-function filterByValue(arr){
-    for(var i = 0; i < arr.length; i++){
-        if(arr[i] === value){
-            return true;
-        }
-    }
-}
 function diff(arr1, arr2) {
     var newArr = [];
-    // Same, same; but different.
     for(var i = 0; i < arr1.length; i++){
-        for (var j = 0; j <arr2.length; j++){
-            if (arr1[i] === arr2[j]) {
+        if(arr2.indexOf(arr1[i]) === -1) {
+            if(newArr.indexOf(arr1[i]) === -1){
                 newArr.push(arr1[i]);
+            }
+        }
+    }
+    for(var j = 0; j < arr2.length; j++){
+        if(arr1.indexOf(arr2[j]) === -1){
+            if(newArr.indexOf(arr2[j]) === -1){
+                newArr.push(arr2[j]);
             }
         }
     }
@@ -24,3 +23,8 @@ function diff(arr1, arr2) {
 }
 
 diff([1, 2, 3, 5], [1, 2, 3, 4, 5]);
+diff(["diorite", "andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"]);
+diff(["andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"]);
+diff(["andesite", "grass", "dirt", "dead shrub"], ["andesite", "grass", "dirt", "dead shrub"]);
+diff([1, "calf", 3, "piglet"], [1, "calf", 3, 4]);
+diff([], ["snuffleupagus", "cookie monster", "elmo"]);
